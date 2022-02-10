a21c90d99866b23fb078c6422b87ff0c2298ed77
Kermilov <ermilov.klim@mail.ru>
2022-02-10 19:43:12 +0300

    03

--- Main/src/Redux/State.js
+++ Main/src/Redux/State.js
@@ -1,39 +1,52 @@
-import {rerenderEntireTree} from "../render";
+let store  = {
+  _state: {
+    messagesPage: {
+      messageData: [
+        {id: 1, name: "Andrew"},
+        {id: 2, name: "Maxim"},
+        {id: 3, name: "Vitaly"},
+        {id: 4, name: "Nastya"}
+      ],
+      dialogsData: [
+        {id: 1, name: "Andrew", message: "Hello"},
+        {id: 2, name: "Maxim", message: "Hey"},
+        {id: 3, name: "Vitaly", message: "Hi"},
+        {id: 4, name: "Nastya", message: "lol"}
+      ]
+    },
 
-let state = {
-  messagesPage: {
-    messageData: [
-      {id: 1, name: "Andrew"},
-      {id: 2, name: "Maxim"},
-      {id: 3, name: "Vitaly"},
-      {id: 4, name: "Nastya"}
-    ],
-    dialogsData: [
-      {id: 1, name: "Andrew", message: "Hello"},
-      {id: 2, name: "Maxim", message: "Hey"},
-      {id: 3, name: "Vitaly", message: "Hi"},
-      {id: 4, name: "Nastya", message: "lol"}
-    ]
+    profilePage: {
+      postsData: [
+        {id: 1, message: "Hi!", likes: "12"},
+        {id: 2, message: "Hello!", likes: "34"},
+        {id: 3, message: "Yo!", likes: "2"},
+        {id: 4, message: "Wazzup!", likes: "10"}
+        ]
+      }
+    },
+
+  getState() {
+    return this._state;
   },
 
-  profilePage: {
-    postsData: [
-      {id: 1, message: "Hi!", likes: "12"},
-      {id: 2, message: "Hello!", likes: "34"},
-      {id: 3, message: "Yo!", likes: "2"},
-      {id: 4, message: "Wazzup!", likes: "10"}
-    ]
+  _rerenderEntireTree() {
+    console.log('Changed');
+    },
+
+  addPost(postMessage) {
+    let newPost = {
+      id: 5,
+      message: postMessage,
+      likes: 0
+    };
+    this._state.profilePage.postsData.push(newPost);
+    this._rerenderEntireTree(this._state);
+  },
+
+  subscribe(observer) {
+    this._rerenderEntireTree = observer;
   }
+
 }
 
-export let addPost = (postMessage) => {
-  let newPost = {
-    id: 5,
-    message: postMessage,
-    likes: 0
-  }
-  state.profilePage.postsData.push(newPost);
-  rerenderEntireTree();
-}
-
-export default state;
+export default store;
--- Main/src/index.js
+++ Main/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {rerenderEntireTree} from "./render";
+import store from './Redux/State';
 
-rerenderEntireTree();
+
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App state={store.getState()} addPost={store.addPost.bind(store)} />
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
 
 reportWebVitals();
